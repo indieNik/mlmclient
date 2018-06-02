5c6bba14639ffb6949744f34cfa7bf292e04ce2f
indieNik <niki.thrill@gmail.com>
2018-06-02 05:58:25 +0530

    Added New Sign Up Method with Password

--- app/routes/login.js
+++ app/routes/login.js
@@ -38,6 +38,40 @@ export default Route.extend({
                 _this.transitionTo("user");
             });
         },
+
+        signInWithPassword: function(provider, email, password) {
+            let _this = this;
+            if(provider == "google") {
+                this.get('session').open('firebase', {provider: 'google'}).then(function(data) {
+                    let userInFirebase = _this.store.query('user', {
+                        orderBy: 'userEmail',
+                        equalTo: data.currentUser.email
+                    });
+                    userInFirebase.then(result => {
+                        if(result.get("length") == 0) {
+                            let newUser = _this.store.createRecord("user");
+                            newUser.set("userName", data.currentUser.displayName);
+                            newUser.set("userEmail", data.currentUser.email);
+                            newUser.set("userImage", data.currentUser.photoURL);
+                            newUser.set("isAdmin", false);
+                            newUser.save();
+                        }
+                    })
+                    _this.transitionTo("index");
+                });
+            }
+            if(provider == "password") {
+                this.get('session').open('firebase', {
+                    provider: 'password',
+                    email: email,
+                    password: password
+                }).then(function(data) {
+                    _this.transitionTo("index");
+                }).catch( error => {
+                    alert("Invalid Password. Try again!");
+                });
+            };
+        },
         
         signOut: function() {
             this.get('session').close();
