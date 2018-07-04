61bd6e2ebb38f47cff31cc0c160326da80beaf67
indieNik <niki.thrill@gmail.com>
2018-07-05 00:09:23 +0530

    Cleaned unnecessary code in Profile View

--- app/routes/user/profile.js
+++ app/routes/user/profile.js
@@ -1,13 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-
-    beforeModel: function() {
-        this._super(...arguments);
-        return this.get('session').fetch().catch(function() {
-        });
-    },
 
     model() {
         return this.store.query('user', {
